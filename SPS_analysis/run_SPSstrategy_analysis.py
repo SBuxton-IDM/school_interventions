faef2ee9a28efb3fd76b2d289567131d86dd7172
Jamie Cohen <jamie.alex.cohen@gmail.com>
2020-07-16 21:05:56 -0400

    time series output included

--- SPS_analysis/run_SPSstrategy_analysis.py
+++ SPS_analysis/run_SPSstrategy_analysis.py
@@ -29,9 +29,13 @@ def school_dict(msims, school_dicts, index):
             school_results[label]['num_staff_test_pos'] = []
             school_results[label]['num_staff_screen_pos'] = []
             school_results[label]['num_staff_cases'] = []
-            
+
+            school_results[label]['num_students'] = []
+            school_results[label]['num_students_tested'] = []
+            school_results[label]['num_students_test_pos'] = []
             school_results[label]['num_students_screen_pos'] = []
             school_results[label]['num_student_cases'] = []
+
             for j, sub_sim in enumerate(sim.sims):
                 school_results[label]['num_teachers_tested'].append(sub_sim.school_info['num_teachers_tested'])
                 school_results[label]['num_teachers_test_pos'].append(sub_sim.school_info['num_teachers_test_pos'])
@@ -44,9 +48,12 @@ def school_dict(msims, school_dicts, index):
                 school_results[label]['num_staff_screen_pos'].append(sub_sim.school_info['num_staff_screen_pos'])
                 school_results[label]['num_staff_cases'].append(sub_sim.school_info['num_staff_cases'])
                 school_results[label]['num_staff'].append(sub_sim.school_info['num_staff'])
-                
+
+                school_results[label]['num_students_tested'].append(sub_sim.school_info['num_students_tested'])
+                school_results[label]['num_students_test_pos'].append(sub_sim.school_info['num_students_test_pos'])
                 school_results[label]['num_students_screen_pos'].append(sub_sim.school_info['num_students_screen_pos'])
                 school_results[label]['num_student_cases'].append(sub_sim.school_info['num_student_cases'])
+                school_results[label]['num_students'].append(sub_sim.school_info['num_students'])
         else:
             for sub_sim in sim.sims:
                 school_results[label]['num_teachers_tested'] = sub_sim.school_info['num_teachers_tested']
@@ -61,8 +68,11 @@ def school_dict(msims, school_dicts, index):
                 school_results[label]['num_staff_cases'] = sub_sim.school_info['num_staff_cases']
                 school_results[label]['num_staff'] = sub_sim.school_info['num_staff']
 
+                school_results[label]['num_students_tested'] = sub_sim.school_info['num_students_tested']
+                school_results[label]['num_students_test_pos'] = sub_sim.school_info['num_students_test_pos']
                 school_results[label]['num_students_screen_pos'] = sub_sim.school_info['num_students_screen_pos']
                 school_results[label]['num_student_cases'] = sub_sim.school_info['num_student_cases']
+                school_results[label]['num_students'] = sub_sim.school_info['num_students']
 
     for _, results in school_results.items():
         for key, value in results.items():
@@ -84,6 +94,7 @@ def school_dict(msims, school_dicts, index):
     school_dicts[label][index] = school_results
     return school_results
 
+
 def process_school_dicts(school_dicts, n_params, scenarios):
     df_list = []
     for scen in scenarios:
@@ -103,7 +114,7 @@ if __name__ == "__main__":
 
     do_save = True
     n_params = 1
-    n_seeds = 3
+    n_seeds = 1
     date = '07162020'
 
 
@@ -120,10 +131,10 @@ if __name__ == "__main__":
                         }
 
     teacher_testing_scens = {
-                        'no_teacher_testing': {'test_freq': None},
+                        # 'no_teacher_testing': {'test_freq': None},
                         'monthly_teacher_testing': {'test_freq': 30},
-                        'biweekly_teacher_testing': {'test_freq': 14},
-                        'weekly_teacher_testing': {'test_freq': 7},
+                        # 'biweekly_teacher_testing': {'test_freq': 14},
+                        # 'weekly_teacher_testing': {'test_freq': 7},
                         }
 
     indices = range(n_params)
@@ -135,6 +146,7 @@ if __name__ == "__main__":
     json = sc.loadjson(jsonfile)
 
     school_dicts = dict()
+    infectious_by_scen = []
     for scen, teacher_testing in teacher_testing_scens.items():
         analysis_name = f'teacher_testing_analysis_{scen}'
         msims = []
@@ -142,7 +154,7 @@ if __name__ == "__main__":
             all_sims = []
             entry = json[index]
             pars = entry['pars']
-            pars['end_day'] = '2020-12-01'
+            pars['end_day'] = '2020-10-01'
             for seed in seeds:
                 pars['rand_seed'] = seed
                 all_sims.append(cs.create_sim(pars=pars, label=scen, school_reopening_pars=school_reopening_pars,
@@ -152,20 +164,33 @@ if __name__ == "__main__":
             msim.run(reseed=False, par_args={'maxload': 0.8}, noise=0.0, keep_people=False)
             msim.reduce()
             df = []
+            infectious = []
             for j in range(len(msim.sims)):
                 df.append(pd.DataFrame(msim.sims[j].results))
+                infectious.append(pd.DataFrame(msim.sims[j].school_info['num_infectious']))
             df_concat = pd.concat(df)
+            infectious_concat = pd.concat(infectious)
             by_row_index = df_concat.groupby(df_concat.index)
             df_means = by_row_index.mean()
+            by_row_index = infectious_concat.groupby(infectious_concat.index)
+            infectious_means = by_row_index.mean()
+            infectious_means.columns = [scen]
+            infectious_by_scen.append(infectious_means)
             msims.append(msim)
 
             if do_save:
                 # filename = f'results/{analysis_name}_param{index}_results.csv'
                 # df_means.to_csv(filename, header=True)
+                # filename = f'results/{analysis_name}_param{index}_infectious.csv'
+                # infectious_means.to_csv(filename, header=True)
                 school_results = school_dict(msims, school_dicts, index)
                 # filename = f'results/{analysis_name}_param{index}_output.csv'
                 # school_results.to_csv(filename, header=True)
+    infectious_by_scen = pd.concat(infectious_by_scen, ignore_index=True, axis=1)
+    infectious_by_scen.columns = teacher_testing_scens.keys()
 
+    filename = f'results/teacher_testing_analysis_infectious_{date}.csv'
+    infectious_by_scen.to_csv(filename, header=True)
     final_results = process_school_dicts(school_dicts, n_params, teacher_testing_scens.keys())
     filename = f'results/teacher_testing_analysis_combined_output_{date}.csv'
     final_results.to_csv(filename, header=True)
