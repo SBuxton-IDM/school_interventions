c0ac59b1d16a5138862ad7daf1373f5c93ef1f3f
Jamie Cohen <jamie.alex.cohen@gmail.com>
2020-07-18 17:07:58 -0400

    some more output for debugging

--- SPS_analysis/run_SPSstrategy_analysis.py
+++ SPS_analysis/run_SPSstrategy_analysis.py
@@ -36,6 +36,12 @@ def school_dict(msims, school_dicts, index):
             school_results[label]['num_students_screen_pos'] = []
             school_results[label]['num_student_cases'] = []
 
+            school_results[label]['cum_infectious_staff'] = []
+            school_results[label]['cum_infectious_students'] = []
+            school_results[label]['cum_asymp_staff'] = []
+            school_results[label]['cum_asymp_students'] = []
+
+
             for j, sub_sim in enumerate(sim.sims):
                 school_results[label]['num_teachers_tested'].append(sub_sim.school_info['num_teachers_tested'])
                 school_results[label]['num_teachers_test_pos'].append(sub_sim.school_info['num_teachers_test_pos'])
@@ -54,6 +60,11 @@ def school_dict(msims, school_dicts, index):
                 school_results[label]['num_students_screen_pos'].append(sub_sim.school_info['num_students_screen_pos'])
                 school_results[label]['num_student_cases'].append(sub_sim.school_info['num_student_cases'])
                 school_results[label]['num_students'].append(sub_sim.school_info['num_students'])
+
+                school_results[label]['cum_infectious_staff'].append(sum(sub_sim.school_info['num_staff_infectious']))
+                school_results[label]['cum_infectious_students'].append(sum(sub_sim.school_info['num_students_infectious']))
+                school_results[label]['cum_asymp_staff'].append(sum(sub_sim.school_info['num_staff_asymptomatic']))
+                school_results[label]['cum_asymp_students'].append(sum(sub_sim.school_info['num_students_asymptomatic']))
         else:
             for sub_sim in sim.sims:
                 school_results[label]['num_teachers_tested'] = sub_sim.school_info['num_teachers_tested']
@@ -73,6 +84,11 @@ def school_dict(msims, school_dicts, index):
                 school_results[label]['num_students_screen_pos'] = sub_sim.school_info['num_students_screen_pos']
                 school_results[label]['num_student_cases'] = sub_sim.school_info['num_student_cases']
                 school_results[label]['num_students'] = sub_sim.school_info['num_students']
+
+                school_results[label]['cum_infectious_staff'] = sum(sub_sim.school_info['num_staff_infectious'])
+                school_results[label]['cum_infectious_students'] = sum(sub_sim.school_info['num_students_infectious'])
+                school_results[label]['cum_asymp_staff'] = sum(sub_sim.school_info['num_staff_asymptomatic'])
+                school_results[label]['cum_asymp_students'] = sum(sub_sim.school_info['num_students_asymptomatic'])
 
     for _, results in school_results.items():
         for key, value in results.items():
@@ -126,20 +142,19 @@ if __name__ == "__main__":
                         'school_start_day': '2020-09-01',
                         'intervention_start_day': {'pk': None, 'es': '2020-09-01', 'ms': '2020-09-01', 'hs': '2020-09-01', 'uv': None},
                         'ttq_scen': 'medium',
-                        'mobility_scens': '80_perc',
-                        'mobility_file': f'inputs/KC_weeklyinteractions_070120_80perc.csv'
+                        'mobility_scens': '90_perc',
+                        'mobility_file': f'inputs/KC_weeklyinteractions_070120_90perc.csv'
                         }
 
     teacher_testing_scens = {
                         'no_teacher_testing': {'test_freq': None},
                         # 'monthly_teacher_testing': {'test_freq': 28},
-                        # 'biweekly_teacher_testing': {'test_freq': 14},
-                        # 'weekly_teacher_testing': {'test_freq': 7},
+                        'biweekly_teacher_testing': {'test_freq': 14},
+                        'weekly_teacher_testing': {'test_freq': 7},
                         'daily_teacher_testing': {'test_freq': 1},
                         }
 
     indices = range(n_params)
-    seeds = range(n_seeds)
     seeds = []
     for x in range(n_seeds):
         seeds.append(random.randint(1,100))
@@ -147,17 +162,19 @@ if __name__ == "__main__":
     json = sc.loadjson(jsonfile)
 
     school_dicts = dict()
-    infectious_by_scen = []
-    asymptomatic_by_scen = []
+    infectious_staff_by_scen = []
+    infectious_students_by_scen = []
+    asymptomatic_staff_by_scen = []
+    asymptomatic_students_by_scen = []
     diagnosed_by_scen = []
-    undiagnosed_by_scen = []
     for scen, teacher_testing in teacher_testing_scens.items():
         analysis_name = f'teacher_testing_analysis_{scen}'
         msims = []
-        infectious_by_param = []
-        asymptomatic_by_param = []
+        infectious_staff_by_param = []
+        infectious_students_by_param = []
+        asymptomatic_staff_by_param = []
+        asymptomatic_students_by_param = []
         diagnosed_by_param = []
-        undiagnosed_by_param = []
         for index in indices:
             all_sims = []
             entry = json[index]
@@ -171,43 +188,51 @@ if __name__ == "__main__":
             msim.run(reseed=False, par_args={'maxload': 0.8}, noise=0.0, keep_people=False)
             msim.reduce()
             df = []
-            infectious = []
-            asymptomatic = []
+            infectious_staff = []
+            infectious_students = []
+            asymptomatic_staff = []
+            asymptomatic_students = []
             diagnosed = []
-            undiagnosed = []
             for j in range(len(msim.sims)):
                 df.append(pd.DataFrame(msim.sims[j].results))
-                infectious.append(pd.DataFrame(msim.sims[j].school_info['num_infectious']))
-                asymptomatic.append(pd.DataFrame(msim.sims[j].school_info['num_asymptomatic']))
+                infectious_staff.append(pd.DataFrame(msim.sims[j].school_info['num_staff_infectious']))
+                infectious_students.append(pd.DataFrame(msim.sims[j].school_info['num_students_infectious']))
+                asymptomatic_staff.append(pd.DataFrame(msim.sims[j].school_info['num_staff_asymptomatic']))
+                asymptomatic_students.append(pd.DataFrame(msim.sims[j].school_info['num_students_asymptomatic']))
                 diagnosed.append(pd.DataFrame(msim.sims[j].school_info['num_diagnosed']))
-                undiagnosed.append(pd.DataFrame(msim.sims[j].school_info['num_undiagnosed']))
             df_concat = pd.concat(df)
             by_row_index = df_concat.groupby(df_concat.index)
             df_means = by_row_index.mean()
 
-            infectious_concat = pd.concat(infectious)
-            by_row_index = infectious_concat.groupby(infectious_concat.index)
-            infectious_means = by_row_index.mean()
-            infectious_means.columns = [scen]
-            infectious_by_param.append(infectious_means)
+            infectious_staff_concat = pd.concat(infectious_staff)
+            by_row_index = infectious_staff_concat.groupby(infectious_staff_concat.index)
+            infectious_staff_means = by_row_index.mean()
+            infectious_staff_means.columns = [scen]
+            infectious_staff_by_param.append(infectious_staff_means)
 
-            asymptomatic_concat = pd.concat(asymptomatic)
-            by_row_index = asymptomatic_concat.groupby(asymptomatic_concat.index)
-            asymptomatic_means = by_row_index.mean()
-            asymptomatic_means.columns = [scen]
-            asymptomatic_by_param.append(asymptomatic_means)
+            infectious_students_concat = pd.concat(infectious_students)
+            by_row_index = infectious_students_concat.groupby(infectious_students_concat.index)
+            infectious_students_means = by_row_index.mean()
+            infectious_students_means.columns = [scen]
+            infectious_students_by_param.append(infectious_students_means)
+
+            asymptomatic_staff_concat = pd.concat(asymptomatic_staff)
+            by_row_index = asymptomatic_staff_concat.groupby(asymptomatic_staff_concat.index)
+            asymptomatic_staff_means = by_row_index.mean()
+            asymptomatic_staff_means.columns = [scen]
+            asymptomatic_staff_by_param.append(asymptomatic_staff_means)
+
+            asymptomatic_students_concat = pd.concat(asymptomatic_students)
+            by_row_index = asymptomatic_students_concat.groupby(asymptomatic_students_concat.index)
+            asymptomatic_students_means = by_row_index.mean()
+            asymptomatic_students_means.columns = [scen]
+            asymptomatic_students_by_param.append(asymptomatic_students_means)
 
             diagnosed_concat = pd.concat(diagnosed)
             by_row_index = diagnosed_concat.groupby(diagnosed_concat.index)
             diagnosed_means = by_row_index.mean()
             diagnosed_means.columns = [scen]
             diagnosed_by_param.append(diagnosed_means)
-
-            undiagnosed_concat = pd.concat(undiagnosed)
-            by_row_index = undiagnosed_concat.groupby(undiagnosed_concat.index)
-            undiagnosed_means = by_row_index.mean()
-            undiagnosed_means.columns = [scen]
-            undiagnosed_by_param.append(undiagnosed_means)
 
             msims.append(msim)
 
@@ -216,49 +241,60 @@ if __name__ == "__main__":
                 df_means.to_csv(filename, header=True)
                 school_results = school_dict(msims, school_dicts, index)
 
-        infectious_concat = pd.concat(infectious_by_param)
-        by_row_index = infectious_concat.groupby(infectious_concat.index)
-        infectious_means = by_row_index.mean()
-        infectious_by_scen.append(infectious_means)
+        infectious_students_concat = pd.concat(infectious_students_by_param)
+        by_row_index = infectious_students_concat.groupby(infectious_students_concat.index)
+        infectious_students_means = by_row_index.mean()
+        infectious_students_by_scen.append(infectious_students_means)
 
-        asymptomatic_concat = pd.concat(asymptomatic_by_param)
-        by_row_index = asymptomatic_concat.groupby(asymptomatic_concat.index)
-        asymptomatic_means = by_row_index.mean()
-        asymptomatic_by_scen.append(asymptomatic_means)
+        infectious_staff_concat = pd.concat(infectious_staff_by_param)
+        by_row_index = infectious_staff_concat.groupby(infectious_staff_concat.index)
+        infectious_staff_means = by_row_index.mean()
+        infectious_staff_by_scen.append(infectious_staff_means)
+        
+        asymptomatic_staff_concat = pd.concat(asymptomatic_staff_by_param)
+        by_row_index = asymptomatic_staff_concat.groupby(asymptomatic_staff_concat.index)
+        asymptomatic_staff_means = by_row_index.mean()
+        asymptomatic_staff_by_scen.append(asymptomatic_staff_means)
+
+        asymptomatic_students_concat = pd.concat(asymptomatic_students_by_param)
+        by_row_index = asymptomatic_students_concat.groupby(asymptomatic_students_concat.index)
+        asymptomatic_students_means = by_row_index.mean()
+        asymptomatic_students_by_scen.append(asymptomatic_students_means)
 
         diagnosed_concat = pd.concat(diagnosed_by_param)
         by_row_index = diagnosed_concat.groupby(diagnosed_concat.index)
         diagnosed_means = by_row_index.mean()
         diagnosed_by_scen.append(diagnosed_means)
 
-        undiagnosed_concat = pd.concat(undiagnosed_by_param)
-        by_row_index = undiagnosed_concat.groupby(undiagnosed_concat.index)
-        undiagnosed_means = by_row_index.mean()
-        undiagnosed_by_scen.append(undiagnosed_means)
+    infectious_staff_by_scen = pd.concat(infectious_staff_by_scen, ignore_index=True, axis=1)
+    infectious_staff_by_scen.columns = teacher_testing_scens.keys()
 
-    infectious_by_scen = pd.concat(infectious_by_scen, ignore_index=True, axis=1)
-    infectious_by_scen.columns = teacher_testing_scens.keys()
+    infectious_students_by_scen = pd.concat(infectious_students_by_scen, ignore_index=True, axis=1)
+    infectious_students_by_scen.columns = teacher_testing_scens.keys()
 
-    asymptomatic_by_scen = pd.concat(asymptomatic_by_scen, ignore_index=True, axis=1)
-    asymptomatic_by_scen.columns = teacher_testing_scens.keys()
+    asymptomatic_staff_by_scen = pd.concat(asymptomatic_staff_by_scen, ignore_index=True, axis=1)
+    asymptomatic_staff_by_scen.columns = teacher_testing_scens.keys()
+
+    asymptomatic_students_by_scen = pd.concat(asymptomatic_students_by_scen, ignore_index=True, axis=1)
+    asymptomatic_students_by_scen.columns = teacher_testing_scens.keys()
 
     diagnosed_by_scen = pd.concat(diagnosed_by_scen, ignore_index=True, axis=1)
     diagnosed_by_scen.columns = teacher_testing_scens.keys()
 
-    undiagnosed_by_scen = pd.concat(undiagnosed_by_scen, ignore_index=True, axis=1)
-    undiagnosed_by_scen.columns = teacher_testing_scens.keys()
-
-    filename = f'results/teacher_testing_analysis_infectious_{date}.csv'
-    infectious_by_scen.to_csv(filename, header=True)
-
-    filename = f'results/teacher_testing_analysis_asymptomatic_{date}.csv'
-    asymptomatic_by_scen.to_csv(filename, header=True)
-
-    filename = f'results/teacher_testing_analysis_diagnosed_{date}.csv'
-    diagnosed_by_scen.to_csv(filename, header=True)
-
-    filename = f'results/teacher_testing_analysis_undiagnosed_{date}.csv'
-    undiagnosed_by_scen.to_csv(filename, header=True)
+    # filename = f'results/teacher_testing_analysis_infectious_staff_{date}.csv'
+    # infectious_staff_by_scen.to_csv(filename, header=True)
+    #
+    # filename = f'results/teacher_testing_analysis_infectious_students_{date}.csv'
+    # infectious_students_by_scen.to_csv(filename, header=True)
+    #
+    # filename = f'results/teacher_testing_analysis_asymptomatic_staff_{date}.csv'
+    # asymptomatic_staff_by_scen.to_csv(filename, header=True)
+    #
+    # filename = f'results/teacher_testing_analysis_asymptomatic_students_{date}.csv'
+    # asymptomatic_students_by_scen.to_csv(filename, header=True)
+    #
+    # filename = f'results/teacher_testing_analysis_diagnosed_{date}.csv'
+    # diagnosed_by_scen.to_csv(filename, header=True)
 
     final_results = process_school_dicts(school_dicts, n_params, teacher_testing_scens.keys())
     filename = f'results/teacher_testing_analysis_combined_output_{date}.csv'
