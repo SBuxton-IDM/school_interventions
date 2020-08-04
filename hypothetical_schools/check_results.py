f5ea795cf1e96e6bcb0b382c9cb66c6c7033485a
cliffckerr <questionably@gmail.com>
2020-08-03 21:05:32 -0700

    much more detail

--- hypothetical_schools/check_results.py
+++ hypothetical_schools/check_results.py
@@ -1,5 +1,6 @@
 import covasim as cv
 import pylab as pl
+import sciris as sc
 
 sdict = cv.load('sdict.obj')
 
@@ -9,14 +10,29 @@ print(sdict['as_normal_20_cases_re_0.9'].school_info['num_student_cases'])
 print(sdict['as_normal_110_cases_re_0.9'].school_info['num_student_cases'])
 
 
-fig = pl.figure(figsize=(15,10), dpi=150)
+fig = pl.figure(figsize=(20,12), dpi=150)
 
-for i,scen in enumerate(['as_normal', 'all_remote']):
-    ax = pl.subplot(2,1,i+1)
-    for label in [f'{scen}_20_cases_re_0.9', f'{scen}_110_cases_re_0.9', f'{scen}_20_cases_re_1.1', f'{scen}_110_cases_re_1.1']:
-        pl.plot(sdict[label].school_info['num_cases_by_day'], label=label, lw=3)
-    pl.legend()
-    pl.title('Number of cases per day (staff + students + teachers)')
+schools_reopening_scenarios = [
+    'as_normal',
+    'with_screening',
+    'with_hybrid_scheduling',
+    'ES_MS_inperson_HS_remote',
+    'ES_inperson_MS_HS_remote',
+    'ES_hybrid',
+    'all_remote',
+]
+
+for i,scen in enumerate(schools_reopening_scenarios):
+    ax = pl.subplot(4,2,i+1)
+    colors = sc.gridcolors(4)
+    factor = 1
+    last = (i == len(schools_reopening_scenarios)-1)
+    for l,label in enumerate([f'{scen}_20_cases_re_0.9', f'{scen}_110_cases_re_0.9', f'{scen}_20_cases_re_1.1', f'{scen}_110_cases_re_1.1']):
+        pl.plot(sdict[label].school_info['num_cases_by_day'], label=label, lw=3, c=colors[l])
+        pl.plot(sdict[label].results['n_infectious'].values/factor, '--', c=colors[l], lw=2, label='Total cases')
+    if last:
+        pl.legend(bbox_to_anchor=(1.1, 1.0))
+    pl.title(f'Number of cases per day (staff + students + teachers) for {scen}')
 
 
 print('Done.')
