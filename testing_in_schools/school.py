27f77cd4d94123cba1b9dedd335dd5ca20ffa7e3
Daniel J. Klein <dklein@idmod.org>
2020-10-06 20:54:32 -0700

    Seems to work!

--- testing_in_schools/school.py
+++ testing_in_schools/school.py
@@ -16,8 +16,8 @@ class FullTimeContactManager():
             'Wednesday': 'all',
             'Thursday':  'all',
             'Friday':    'all',
-            'Saturday':  'no_school',
-            'Sunday':    'no_school',
+            'Saturday':  'weekend',
+            'Sunday':    'weekend',
         }
 
     def begin_day(self, date):
@@ -28,7 +28,7 @@ class FullTimeContactManager():
         # Could modify layer based on group
         if group == 'all':
             # Start with the original layer, will remove uids at home later
-            self.layer = self.base_layer
+            self.layer = sc.dcp(self.base_layer) # needed?
         else:
             self.layer = cvb.Layer() # Empty
 
@@ -56,7 +56,7 @@ class HybridContactManager():
         self.sim = sim
         self.uids = uids
         self.base_layer = layer
-        self.A_layer, self.B_layer = self.split_layer()
+        self.A_base_layer, self.B_base_layer = self.split_layer()
 
         self.group = 'closed'
 
@@ -66,31 +66,30 @@ class HybridContactManager():
             'Wednesday': 'distance',
             'Thursday':  'B',
             'Friday':    'B',
-            'Saturday':  'no_school',
-            'Sunday':    'no_school',
+            'Saturday':  'weekend',
+            'Sunday':    'weekend',
         }
 
     def split_layer(self):
         students = [uid for uid in self.uids if self.sim.people.student_flag[uid]]
-        staff = [uid for uid in self.uids if self.sim.people.staff_flag[uid]]
-        teachers = [uid for uid in self.uids if self.sim.people.teacher_flag[uid]]
+        self.staff = [uid for uid in self.uids if self.sim.people.staff_flag[uid]]
+        self.teachers = [uid for uid in self.uids if self.sim.people.teacher_flag[uid]]
 
-        A_students = cvu.binomial_filter(0.5, np.array(students)).tolist()
-        B_students = [uid for uid in students if uid not in A_students]
+        self.A_students = cvu.binomial_filter(0.5, np.array(students)).tolist()
+        self.B_students = [uid for uid in students if uid not in self.A_students]
 
-        A_group = A_students + teachers + staff
-        B_group = B_students + teachers + staff
+        self.A_group = self.A_students + self.teachers + self.staff
+        self.B_group = self.B_students + self.teachers + self.staff
 
         A_layer = sc.dcp(self.base_layer)
         rows = np.concatenate((
-                np.isin(A_layer['p1'], B_students).nonzero()[0],
-                np.isin(A_layer['p2'], B_students).nonzero()[0]))
-        pop = A_layer.pop_inds(rows)
+                np.isin(A_layer['p1'], self.B_students).nonzero()[0],
+                np.isin(A_layer['p2'], self.B_students).nonzero()[0]))
 
         B_layer = sc.dcp(self.base_layer)
         rows = np.concatenate((
-                np.isin(B_layer['p1'], A_students).nonzero()[0],
-                np.isin(B_layer['p2'], A_students).nonzero()[0]))
+                np.isin(B_layer['p1'], self.A_students).nonzero()[0],
+                np.isin(B_layer['p2'], self.A_students).nonzero()[0]))
         pop = B_layer.pop_inds(rows)
         return A_layer, B_layer
 
@@ -101,29 +100,26 @@ class HybridContactManager():
 
         # Could modify layer based on group
         if group == 'A':
-            self.layer = self.A_layer
+            self.layer = sc.dcp(self.A_base_layer) # needed?
         elif group == 'B':
-            self.layer = self.B_layer
+            self.layer = sc.dcp(self.B_base_layer) # needed?
         else:
             self.layer = cvb.Layer() # Empty
 
     def remove_individuals(self, uids):
         ''' Remove one or more individual from the contact network '''
 
-        print(f'Removing {uids}')
-        for layer in [self.A_layer, self.B_layer]:
-            rows = np.concatenate((
-                np.isin(layer['p1'], uids).nonzero()[0],
-                np.isin(layer['p2'], uids).nonzero()[0]))
-            pop = layer.pop_inds(rows)
+        #print(f'Removing {uids}')
+        rows = np.concatenate((
+            np.isin(self.layer['p1'], uids).nonzero()[0],
+            np.isin(self.layer['p2'], uids).nonzero()[0]))
+        pop = self.layer.pop_inds(rows)
 
     def find_contacts(self, uids):
-        # Assumption: base_layer is close enough, not adjusting for absenteeism
-
-        # Look in A layer for A layer friends, B for B layer friends
-        #A_uids = _____
-        #B_uids = _____
-        return self.base_layer.find_contacts(uids)
+        contacts = np.concatenate((
+            self.A_base_layer.find_contacts(uids),
+            self.B_base_layer.find_contacts(uids) ))
+        return contacts
 
     def get_layer(self):
         return self.layer
@@ -162,19 +158,19 @@ class School():
 
         # Inclusion criteria: diagnosed or symptomatic but not recovered and not dead
         dx_or_sx = np.logical_or(
-            self.sim.people.diagnosed[self.uids_at_school],
-            self.sim.people.symptomatic[self.uids_at_school])
+            self.sim.people.diagnosed[self.uids_arriving_at_school],
+            self.sim.people.symptomatic[self.uids_arriving_at_school])
 
         rec_or_dead = np.logical_or(
-            self.sim.people.recovered[self.uids_at_school],
-            self.sim.people.dead[self.uids_at_school])
+            self.sim.people.recovered[self.uids_arriving_at_school],
+            self.sim.people.dead[self.uids_arriving_at_school])
 
         screen_pos = np.logical_and(dx_or_sx, ~rec_or_dead)
-        screen_pos_uids = cvu.itrue(screen_pos, np.array(self.uids_at_school))
+        screen_pos_uids = cvu.itrue(screen_pos, np.array(self.uids_arriving_at_school))
 
         # Add in screen positives from ILI
         if self.ili_prob is not None and self.ili_prob > 0:
-            screen_neg_uids = np.array(self.uids_at_school)[~screen_pos]
+            screen_neg_uids = np.array(self.uids_arriving_at_school)[~screen_pos]
             n_ili = np.random.binomial(len(screen_neg_uids), self.ili_prob) # Poisson
             if n_ili > 0:
                 ili_pos_uids = np.random.choice(screen_neg_uids, n_ili, replace=False)
@@ -196,27 +192,6 @@ class School():
         date = self.sim.date(self.sim.t)
         self.ct_mgr.begin_day(date) # Do this at the beginning of the update
 
-        if self.ct_mgr.group in ['no_school', 'distance']:
-            return cvb.Layer() # No school today
-
-        # If any individuals are done with quarantine, return them to school
-        self.uids_at_home = {uid:date for uid,date in self.uids_at_home.items() if date > self.sim.t}
-
-        # Everyone else is at school
-        self.uids_at_school = [u for u in self.uids if u not in self.uids_at_home.keys()]
-
-        # Perform symptom screening
-        screen_pos_ids = self.screen()
-
-        if len(screen_pos_ids) > 0:
-            # Send the screen positives home
-            for uid in screen_pos_ids:
-                self.uids_at_home[uid] = self.sim.t + 2 # Can come back in a few days
-
-            # Perform follow-up testing on some
-            uids_to_test = cvu.binomial_filter(self.test_prob, screen_pos_ids)
-            self.sim.people.test(uids_to_test, test_delay=1) # one day test delay, TODO: Make a parameter!
-
         # Look for newly diagnosed people
         newly_dx_inds = cvu.itrue(self.sim.people.date_diagnosed[self.uids] == self.sim.t, np.array(self.uids)) # Diagnosed this time step, time to trace
 
@@ -232,6 +207,28 @@ class School():
             # Quarantine school contacts
             for uid in uids_to_quar:
                 self.uids_at_home[uid] = self.sim.t + self.sim.pars['quar_period'] # Can come back after quarantine period
+
+        # If any individuals are done with quarantine, return them to school
+        self.uids_at_home = {uid:date for uid,date in self.uids_at_home.items() if date > self.sim.t}
+
+        if self.ct_mgr.group in ['weekend', 'distance']:
+            # No school today, nothing more to do - return an empty layer
+            return cvb.Layer()
+
+        # Determine who will arrive at school
+        self.uids_arriving_at_school = [u for u in self.uids if u not in self.uids_at_home.keys()]
+
+        # Perform symptom screening
+        screen_pos_ids = self.screen()
+
+        if len(screen_pos_ids) > 0:
+            # Send the screen positives home
+            for uid in screen_pos_ids:
+                self.uids_at_home[uid] = self.sim.t + 2 # Can come back in a few days, TODO: make a parameter
+
+            # Perform follow-up testing on some
+            uids_to_test = cvu.binomial_filter(self.test_prob, screen_pos_ids)
+            self.sim.people.test(uids_to_test, test_delay=1) # one day test delay, TODO: Make a parameter!
 
         # Remove individuals at home from the network
         self.ct_mgr.remove_individuals(list(self.uids_at_home.keys()))
