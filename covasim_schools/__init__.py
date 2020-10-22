080fa64b7d69a9dda60a88e2d22b9f80515ea8c5
cliffckerr <questionably@gmail.com>
2020-10-21 21:51:40 -0700

    beginning transition into python module

---
+++ covasim_schools/__init__.py
@@ -0,0 +1,7 @@
+from .version import __version__, __versiondate__
+from .utils import *
+from .problem_suite import *
+from .sim_suite import *
+from .shell_step import *
+from .binnts import *
+from .stochastic_descent import *
