32ac93568cd5611bea102fdcb8a4086d6874a58a
cliffckerr <questionably@gmail.com>
2020-10-25 10:01:21 -0700

    plot population

--- tests/test_pop.py
+++ tests/test_pop.py
@@ -4,9 +4,12 @@ Very simple test of population generation
 
 import covasim_schools as cvsch
 
-def test_school_pop():
+def test_school_pop(do_plot=False):
     pop = cvsch.make_population(pop_size=20e3, rand_seed=1, do_save=False)
+
+    if do_plot:
+        pop.plot()
     return pop
 
 if __name__ == '__main__':
-	pop = test_school_pop()
+	pop = test_school_pop(do_plot=True)
