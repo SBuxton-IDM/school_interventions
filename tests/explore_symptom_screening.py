77f2e14159f65f061b18014a1f3a8093b8a05785
cliffckerr <questionably@gmail.com>
2020-10-26 22:22:21 -0700

    larger population

--- tests/explore_symptom_screening.py
+++ tests/explore_symptom_screening.py
@@ -3,21 +3,17 @@ Not a formal test -- test that symptom screening is doing what it is supposed to
 '''
 
 import os
-import numpy as np
-import pylab as pl
 import sciris as sc
-import seaborn as sns
 import covasim as cv
 import covasim_schools as cvsch
 from testing_in_schools import create_sim as cs
-from testing_in_schools.testing_scenarios import generate_scenarios, generate_testing
+from testing_in_schools.testing_scenarios import generate_scenarios
 
 #%% Configuration
 sc.heading('Configuring...')
 T = sc.tic()
 
 debug       = True # Verobisty and other settings
-bypass      = True # Whether to use a small population size
 do_run      = True # Whether to rerun instead of load saved run
 keep_people = False # Whether to keep people when running
 parallelize = True # If running, whether to parallelize
@@ -26,25 +22,18 @@ do_plot     = True # Whether to plot results
 
 n_seeds = 4 # Number of seeds to run each simulation with
 rand_seed = 2346 # Overwrite the default random seed
-folder = '../testing_in_schools/v20201019'
-bypass_popfile = 'explore_symptoms_small.ppl'
-sims_file = 'explore_scenarios.sims'
-pop_size = int(20e3) if bypass else int(2.25e5)
-calibfile = os.path.join(folder, 'pars_cases_begin=75_cases_end=75_re=1.0_prevalence=0.002_yield=0.024_tests=225_pop_size=225000.json')
+bypass_popfile = 'explore_symptoms_medium.ppl'
+pop_size = int(100e3)
 
-try:
-    entry = sc.loadjson(calibfile)[1]
-except Exception as E:
-    entry =   {
-        "index": 376.0,
-        "mismatch": 0.03221581045452142,
-        "pars": {
-          "pop_infected": 242.11186358945181,
-          "change_beta": 0.5313884845187986,
-          "symp_prob": 0.08250498122080606
-        }
-      }
-    print(f'Warning: could not load calibration file "{calibfile}" due to "{str(E)}", using hard-coded parameters')
+entry =   {
+    "index": 376.0,
+    "mismatch": 0.03221581045452142,
+    "pars": {
+      "pop_infected": 242.11186358945181,
+      "change_beta": 0.5313884845187986,
+      "symp_prob": 0.08250498122080606
+    }
+  }
 params = sc.dcp(entry['pars'])
 if rand_seed is None:
     params['rand_seed'] = int(entry['index'])
@@ -52,24 +41,14 @@ else:
     params['rand_seed'] = rand_seed
 
 # Ensure the population file exists
-if bypass and not os.path.exists(bypass_popfile):
+if not os.path.exists(bypass_popfile):
     print(f'Population file {bypass_popfile} not found, recreating...')
     cvsch.make_population(pop_size=pop_size, rand_seed=params['rand_seed'], max_pop_seeds=5, popfile=bypass_popfile, do_save=True)
 
-# Create the scenarios
-divider = ' -- '
-def joinkeys(skey, tkey):
-    ''' Turn scenario and testing keys into a single key '''
-    return divider.join([skey, tkey])
-
-def splitkey(key):
-    ''' Oppostite of joinkeys() '''
-    return key.split(divider)
-
 origscen = generate_scenarios()['as_normal']
-testings = {'none':
+testings = {'No testing':
                 None,
-            'antigen': {
+            'Antigen testing': {
                 'start_date': '2020-10-26',
                 'repeat': 14,
                 'groups': ['students', 'teachers', 'staff'], # No students
@@ -81,6 +60,7 @@ testings = {'none':
                 'PCR_followup_perc': 1.0,
                 'PCR_followup_delay': 3.0,
             }}
+
 t_keys = list(testings.keys())
 n_testings = len(testings)
 all_scens = sc.odict()
@@ -93,12 +73,8 @@ for tkey,testing in testings.items():
     all_scens[tkey] = scen
 
 # Create the sim
-if bypass: # BYPASS option -- create small population on the fly
-    people = sc.loadobj(bypass_popfile)
-    base_sim = cs.create_sim(params, pop_size=pop_size, load_pop=False, people=people, verbose=0.1)
-else: # Otherwise, load full population from disk
-    base_sim = cs.create_sim(params, pop_size=pop_size, folder=folder, verbose=0.1)
-
+people = sc.loadobj(bypass_popfile)
+base_sim = cs.create_sim(params, pop_size=pop_size, load_pop=False, people=people, verbose=0.1)
 
 #%% Run the sims
 msims = []
@@ -107,7 +83,7 @@ for key,scen in all_scens.items():
     for seed in range(n_seeds):
         sim = sc.dcp(base_sim)
         sim.set_seed(seed=sim['rand_seed'] + seed)
-        sim.label = f'Testing = {key}, seed = {seed}'
+        sim.label = key
         sim['interventions'] += [cvsch.schools_manager(scen)]
         sims.append(sim)
     msim = cv.MultiSim(sims)
